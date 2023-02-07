d8066a57061d3404c65763a404cbd0b60b027552
Saurabh Harak <43716369+saurabhharak@users.noreply.github.com>
2023-02-07 21:26:47 +0530

    file_upload_to_wasabi fn added

--- wasabit/wasabi_upload.py
+++ wasabit/wasabi_upload.py
@@ -85,3 +85,40 @@ def upload_to_wasabi(folder_path: str, bucket_name: str, wasabi_path: str, acces
             print(f'An error occurred: {e}')
     except Exception as e:
         print(f'An error occurred: {e}')
+
+        
+
+def file_upload_to_wasabi(csv_file_path: str, bucket_name: str, wasabi_path: str, access_key = None, secret_key = None) -> None:
+    """
+    Uploads a specific CSV file to a specified Wasabi bucket and subfolder or prefix.
+    :param csv_file_path: The full path of the CSV file to be uploaded.
+    :param bucket_name: The name of the Wasabi bucket where the file will be uploaded.
+    :param wasabi_path: The prefix or subfolder within the bucket where the file will be uploaded.
+    :param access_key: The Wasabi access key.
+    :param secret_key: The Wasabi secret key.
+    """
+    transfer_config = TransferConfig(
+        multipart_threshold=1024 * 25,  # 25MB
+        max_concurrency=10,
+        num_download_attempts=10,
+    )
+    try:
+        # create an S3 client
+        s3 = wasabi_auth()
+        # get the file name from the full path
+        file_name = os.path.basename(csv_file_path)
+        remote_file = wasabi_path + file_name
+        # upload the file
+        s3.upload_file(csv_file_path, bucket_name, remote_file, Config=transfer_config)
+        print(f'{csv_file_path} is uploaded to {remote_file}')
+    except NoCredentialsError as e:
+        print("Credentials not found, please check your access key and secret key.")
+    except ClientError as e:
+        if e.response['Error']['Code'] == "InvalidAccessKeyId":
+            print("Invalid access key, please check your access key.")
+        elif e.response['Error']['Code'] == "SignatureDoesNotMatch":
+            print("Invalid secret key, please check your secret key.")
+        else:
+            print(f'An error occurred: {e}')
+    except Exception as e:
+        print(f'An error occurred: {e}')
