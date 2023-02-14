045bf19c69e1cf187516dcfa59bb17c6586ccfef
Saurabh Harak <43716369+saurabhharak@users.noreply.github.com>
2023-02-14 17:44:08 +0530

    pypi v1

--- setup.py
+++ setup.py
@@ -2,11 +2,16 @@ from setuptools import setup, find_packages
 
 setup(
     name='wasabit',
-    version='0.3',
+    version='0.1',
     description='This package offers a solution for uploading files, creating folders, and authenticating with Wasabi',
     author='Saurabh Harak',
     author_email='saurabh_harak@isb.edu',
     packages=find_packages(),
+    classifiers=[
+         "Programming Language :: Python :: 3",
+         "License :: OSI Approved :: MIT License",
+         "Operating System :: OS Independent",
+     ],
     install_requires=[
         'boto3==1.26.65',
         'botocore==1.29.65',
